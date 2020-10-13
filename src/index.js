69bdfe3538c6102354ebaf99f8510e2501250eed
inessa65 <inessa65@yandex.by>
2020-10-13 12:06:19 +0300

    init:start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack=[];
+  for(let bracket of str){
+      let bracketsIndex = bracketsConfig.indexOf(bracket)
+     if (bracketsIndex === -1){
+ continue;
+    
+   
+        } if
+         (bracketsIndex %2 === 0){
+           stack.push(bracketsIndex +1); 
+        }
+    
+
+         
+     else{
+         if(stack.length === 0 || stack.pop() !==bracketsIndex){
+             return false;
+         }
+     }
+  }
+  return stack.length ===0;
 }
